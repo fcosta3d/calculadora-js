e8228a05b0abe8a7fc365fb6386e2d152eaa2bd7
Flávio <fcosta3d@gmail.com>
2018-08-14 15:54:47 -0300

    Refactory do objeto calculadora

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,22 +1,45 @@
 var Calculadora = {
+  ADICAO: '+',
+  SUBTRACAO: '-',
+  MULTIPLICACAO: '*',
+  DIVISAO: '/',
+
   adicionar(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) return 0;
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    return num1 + num2;
+    return Calculadora.calcular(num1, num2, Calculadora.ADICAO);
   },
+
   subtrair(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) return 0;
-    return num1 - num2
+    return Calculadora.calcular(num1, num2, Calculadora.SUBTRACAO);
   },
+
   dividir(num1, num2) {
     if (num2 == 0) return 'Erro';
-    if (isNaN(num1) || isNaN(num2)) return 0;
-    return num1 / num2
+    return Calculadora.calcular(num1, num2, Calculadora.DIVISAO);
   },
+
   multiplicar(num1, num2) {
     if (isNaN(num1) || isNaN(num2)) return 0;
     return num1 * num2;
+  },
+
+  calcular(num1, num2, operacao) {
+    var resultado = 0;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if (isNaN(num1) || isNaN(num2)) return 0;
+
+    switch (operacao) {
+      case Calculadora.ADICAO:
+        return num1 + num2;
+      case Calculadora.SUBTRACAO:
+        return num1 - num2;
+      case Calculadora.DIVISAO:
+        return num1 / num2;
+      case Calculadora.MULTIPLICACAO:
+        return num1 / num2;
+      default:
+        return 0
+    }
   }
 };
 
